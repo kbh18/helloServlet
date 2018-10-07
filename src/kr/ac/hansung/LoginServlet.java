93b8a8282f8922e077b1b1640a8b86cc15414eb9
KBH <75nstebesb@naver.com>
2018-10-07 23:28:14 +0900

    Modify some text(is )

--- src/kr/ac/hansung/LoginServlet.java
+++ src/kr/ac/hansung/LoginServlet.java
@@ -38,7 +38,7 @@ public class LoginServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		
 		String htmlResponse ="<html>";
-		htmlResponse += "<h2> your name is" + username + "<br/>";
+		htmlResponse += "<h2> your name is is" + username + "<br/>";
 		htmlResponse += "your password is " + password + "</h2>";
 		htmlResponse += "</html>";
 		
